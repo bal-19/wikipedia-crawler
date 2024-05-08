89a1d8062150b22ccb5fdc5c6a92cc072572d707
iqbaal <152265152+bal-19@users.noreply.github.com>
2024-05-08 17:52:43 +0700

    Update connection.py

--- s3/connection.py
+++ s3/connection.py
@@ -4,9 +4,9 @@ import s3fs
 class Connection:
     def __init__(self):
         self.client_kwargs = {
-            'key': 'GLZG2JTWDFFSCQVE7TSQ',
-            'secret': 'VjTXOpbhGvYjDJDAt2PNgbxPKjYA4p4B7Btmm4Tw',
-            'endpoint_url': 'http://192.168.180.9:8000',
+            'key': '',
+            'secret': '',
+            'endpoint_url': '',
             'anon': False
         }
         self.s3 = s3fs.core.S3FileSystem(**self.client_kwargs)
